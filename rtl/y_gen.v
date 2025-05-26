29374064b3815a4902d1d4ea8dabe88836a84bcd
Wishes <2859067860@qq.com>
2025-05-26 15:39:06 +0800

    DC优化0.0011

--- rtl/y_gen.v
+++ rtl/y_gen.v
@@ -155,7 +155,7 @@ always@(posedge clk) begin
     end
 end
 
-//第4级：计算完整的乘法结果
+//第4级：合并部分积
 reg [26:0] a_cos;
 reg [29:0] b_div;
 reg sign_reg_3;
@@ -167,123 +167,75 @@ always@(posedge clk) begin
         sign_reg_3 <= 1'b0;
     end else begin
         sign_reg_3 <= sign_reg_2_5;
-        // 合并部分积得到完整结果
-        a_cos <= a_cos_partial_1 + (a_cos_partial_2 << 8);
-        b_div <= b_div_partial_1 + (b_div_partial_2 << 9);
-    end
-end
-
-//第4.5级：新增流水线级 - 复制信号以减少扇出
-reg [26:0] a_cos_copy1, a_cos_copy2, a_cos_copy3, a_cos_copy4, a_cos_copy5;
-reg [29:0] b_div_copy1, b_div_copy2, b_div_copy3, b_div_copy4, b_div_copy5;
-reg sign_reg_3_5;
-
-always@(posedge clk) begin
-    if(!rst_n) begin
-        a_cos_copy1 <= 27'd0;
-        a_cos_copy2 <= 27'd0;
-        a_cos_copy3 <= 27'd0;
-        a_cos_copy4 <= 27'd0;
-        a_cos_copy5 <= 27'd0;
-        b_div_copy1 <= 30'd0;
-        b_div_copy2 <= 30'd0;
-        b_div_copy3 <= 30'd0;
-        b_div_copy4 <= 30'd0;
-        b_div_copy5 <= 30'd0;
-        sign_reg_3_5 <= 1'b0;
-    end else begin
-        sign_reg_3_5 <= sign_reg_3;
-        // 复制信号，每个副本只驱动5个乘法器
-        a_cos_copy1 <= a_cos;
-        a_cos_copy2 <= a_cos;
-        a_cos_copy3 <= a_cos;
-        a_cos_copy4 <= a_cos;
-        a_cos_copy5 <= a_cos;
-        b_div_copy1 <= b_div;
-        b_div_copy2 <= b_div;
-        b_div_copy3 <= b_div;
-        b_div_copy4 <= b_div;
-        b_div_copy5 <= b_div;
+        // 使用预分解的部分积
+        a_cos <= a_cos_p1_low + {a_cos_p1_high, 8'b0} + 
+                 {a_cos_p2_low, 8'b0} + {a_cos_p2_high, 16'b0};
+        b_div <= b_div_p1_low + {b_div_p1_high, 9'b0} + 
+                 {b_div_p2_low, 9'b0} + {b_div_p2_high, 18'b0};
     end
 end
 
 //第5级：进一步分解乘法，减少每个乘法器的位宽
-// 将27x30位乘法分解为更小的乘法器（6位x6位）
-reg [11:0] mp_1_1, mp_1_2, mp_1_3, mp_1_4, mp_1_5;
-reg [11:0] mp_2_1, mp_2_2, mp_2_3, mp_2_4, mp_2_5;
-reg [11:0] mp_3_1, mp_3_2, mp_3_3, mp_3_4, mp_3_5;
-reg [11:0] mp_4_1, mp_4_2, mp_4_3, mp_4_4, mp_4_5;
-reg [11:0] mp_5_1, mp_5_2, mp_5_3, mp_5_4, mp_5_5;
+// 将27x30位乘法分解为更多的小乘法器
+reg [18:0] mult_partial_1_1, mult_partial_1_2, mult_partial_1_3;
+reg [18:0] mult_partial_2_1, mult_partial_2_2, mult_partial_2_3;
+reg [18:0] mult_partial_3_1, mult_partial_3_2, mult_partial_3_3;
 reg sign_reg_4;
 
 always@(posedge clk) begin
     if(!rst_n) begin
-        mp_1_1 <= 12'd0; mp_1_2 <= 12'd0; mp_1_3 <= 12'd0; mp_1_4 <= 12'd0; mp_1_5 <= 12'd0;
-        mp_2_1 <= 12'd0; mp_2_2 <= 12'd0; mp_2_3 <= 12'd0; mp_2_4 <= 12'd0; mp_2_5 <= 12'd0;
-        mp_3_1 <= 12'd0; mp_3_2 <= 12'd0; mp_3_3 <= 12'd0; mp_3_4 <= 12'd0; mp_3_5 <= 12'd0;
-        mp_4_1 <= 12'd0; mp_4_2 <= 12'd0; mp_4_3 <= 12'd0; mp_4_4 <= 12'd0; mp_4_5 <= 12'd0;
-        mp_5_1 <= 12'd0; mp_5_2 <= 12'd0; mp_5_3 <= 12'd0; mp_5_4 <= 12'd0; mp_5_5 <= 12'd0;
+        mult_partial_1_1 <= 19'd0;
+        mult_partial_1_2 <= 19'd0;
+        mult_partial_1_3 <= 19'd0;
+        mult_partial_2_1 <= 19'd0;
+        mult_partial_2_2 <= 19'd0;
+        mult_partial_2_3 <= 19'd0;
+        mult_partial_3_1 <= 19'd0;
+        mult_partial_3_2 <= 19'd0;
+        mult_partial_3_3 <= 19'd0;
         sign_reg_4 <= 1'b0;
     end else begin
-        sign_reg_4 <= sign_reg_3_5;
-        // 使用不同的副本来减少扇出
-        // 第1行使用copy1
-        mp_1_1 <= a_cos_copy1[5:0]   * b_div_copy1[5:0];
-        mp_1_2 <= a_cos_copy1[5:0]   * b_div_copy1[11:6];
-        mp_1_3 <= a_cos_copy1[5:0]   * b_div_copy1[17:12];
-        mp_1_4 <= a_cos_copy1[5:0]   * b_div_copy1[23:18];
-        mp_1_5 <= a_cos_copy1[5:0]   * b_div_copy1[29:24];
-        
-        // 第2行使用copy2
-        mp_2_1 <= a_cos_copy2[11:6]  * b_div_copy2[5:0];
-        mp_2_2 <= a_cos_copy2[11:6]  * b_div_copy2[11:6];
-        mp_2_3 <= a_cos_copy2[11:6]  * b_div_copy2[17:12];
-        mp_2_4 <= a_cos_copy2[11:6]  * b_div_copy2[23:18];
-        mp_2_5 <= a_cos_copy2[11:6]  * b_div_copy2[29:24];
-        
-        // 第3行使用copy3
-        mp_3_1 <= a_cos_copy3[17:12] * b_div_copy3[5:0];
-        mp_3_2 <= a_cos_copy3[17:12] * b_div_copy3[11:6];
-        mp_3_3 <= a_cos_copy3[17:12] * b_div_copy3[17:12];
-        mp_3_4 <= a_cos_copy3[17:12] * b_div_copy3[23:18];
-        mp_3_5 <= a_cos_copy3[17:12] * b_div_copy3[29:24];
-        
-        // 第4行使用copy4
-        mp_4_1 <= a_cos_copy4[23:18] * b_div_copy4[5:0];
-        mp_4_2 <= a_cos_copy4[23:18] * b_div_copy4[11:6];
-        mp_4_3 <= a_cos_copy4[23:18] * b_div_copy4[17:12];
-        mp_4_4 <= a_cos_copy4[23:18] * b_div_copy4[23:18];
-        mp_4_5 <= a_cos_copy4[23:18] * b_div_copy4[29:24];
-        
-        // 第5行使用copy5
-        mp_5_1 <= {3'b0, a_cos_copy5[26:24]} * b_div_copy5[5:0];
-        mp_5_2 <= {3'b0, a_cos_copy5[26:24]} * b_div_copy5[11:6];
-        mp_5_3 <= {3'b0, a_cos_copy5[26:24]} * b_div_copy5[17:12];
-        mp_5_4 <= {3'b0, a_cos_copy5[26:24]} * b_div_copy5[23:18];
-        mp_5_5 <= {3'b0, a_cos_copy5[26:24]} * b_div_copy5[29:24];
+        sign_reg_4 <= sign_reg_3;
+        // 将a_cos分为3段：[8:0], [17:9], [26:18]
+        // 将b_div分为3段：[9:0], [19:10], [29:20]
+        // 9x10位乘法器
+        mult_partial_1_1 <= a_cos[8:0] * b_div[9:0];
+        mult_partial_1_2 <= a_cos[8:0] * b_div[19:10];
+        mult_partial_1_3 <= a_cos[8:0] * b_div[29:20];
+        mult_partial_2_1 <= a_cos[17:9] * b_div[9:0];
+        mult_partial_2_2 <= a_cos[17:9] * b_div[19:10];
+        mult_partial_2_3 <= a_cos[17:9] * b_div[29:20];
+        mult_partial_3_1 <= a_cos[26:18] * b_div[9:0];
+        mult_partial_3_2 <= a_cos[26:18] * b_div[19:10];
+        mult_partial_3_3 <= a_cos[26:18] * b_div[29:20];
     end
 end
 
 //第5.5级：新增流水线级 - 第一级部分积累加
-reg [29:0] sum_p1, sum_p2, sum_p3, sum_p4, sum_p5;
+reg [56:0] sum_partial_1;
+reg [56:0] sum_partial_2;
+reg [56:0] sum_partial_3;
 reg sign_reg_4_5;
 
 always@(posedge clk) begin
     if(!rst_n) begin
-        sum_p1 <= 30'd0;
-        sum_p2 <= 30'd0;
-        sum_p3 <= 30'd0;
-        sum_p4 <= 30'd0;
-        sum_p5 <= 30'd0;
+        sum_partial_1 <= 57'd0;
+        sum_partial_2 <= 57'd0;
+        sum_partial_3 <= 57'd0;
         sign_reg_4_5 <= 1'b0;
     end else begin
         sign_reg_4_5 <= sign_reg_4;
-        // 5个并行加法器，每个处理一行
-        sum_p1 <= mp_1_1 + {mp_1_2, 6'b0} + {mp_1_3, 12'b0} + {mp_1_4, 18'b0} + {mp_1_5, 24'b0};
-        sum_p2 <= mp_2_1 + {mp_2_2, 6'b0} + {mp_2_3, 12'b0} + {mp_2_4, 18'b0} + {mp_2_5, 24'b0};
-        sum_p3 <= mp_3_1 + {mp_3_2, 6'b0} + {mp_3_3, 12'b0} + {mp_3_4, 18'b0} + {mp_3_5, 24'b0};
-        sum_p4 <= mp_4_1 + {mp_4_2, 6'b0} + {mp_4_3, 12'b0} + {mp_4_4, 18'b0} + {mp_4_5, 24'b0};
-        sum_p5 <= mp_5_1 + {mp_5_2, 6'b0} + {mp_5_3, 12'b0} + {mp_5_4, 18'b0} + {mp_5_5, 24'b0};
+        // 按照正确的位移权重累加
+        // mult_partial_x_y: x表示a_cos段，y表示b_div段
+        sum_partial_1 <= mult_partial_1_1;  // [8:0] * [9:0] 
+        sum_partial_2 <= {mult_partial_1_2, 10'b0} +      // [8:0] * [19:10] << 10
+                        {mult_partial_2_1, 9'b0} +        // [17:9] * [9:0] << 9
+                        {mult_partial_1_3, 20'b0};        // [8:0] * [29:20] << 20
+        sum_partial_3 <= {mult_partial_2_2, 19'b0} +      // [17:9] * [19:10] << 19
+                        {mult_partial_3_1, 18'b0} +       // [26:18] * [9:0] << 18
+                        {mult_partial_2_3, 29'b0} +       // [17:9] * [29:20] << 29
+                        {mult_partial_3_2, 28'b0} +       // [26:18] * [19:10] << 28
+                        {mult_partial_3_3, 38'b0};        // [26:18] * [29:20] << 38
     end
 end
 
@@ -299,9 +251,9 @@ always@(posedge clk) begin
         sign_reg_5 <= 1'b0;
     end else begin
         sign_reg_5 <= sign_reg_4_5;
-        // 合并5个部分积，按照正确的位移权重
-        result_partial_1 <= {27'd0, sum_p1} + {21'd0, sum_p2, 6'b0};
-        result_partial_2 <= {15'd0, sum_p3, 12'b0} + {9'd0, sum_p4, 18'b0} + {3'd0, sum_p5, 24'b0};
+        // 合并3个部分积
+        result_partial_1 <= sum_partial_1 + sum_partial_2;
+        result_partial_2 <= sum_partial_3;
     end
 end
 
